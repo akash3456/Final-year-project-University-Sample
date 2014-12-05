8a577620d391aef130d6eb55322a6b5fcb302c95
Akash Paul <akashpaul0210@gmail.com>
2014-12-05 12:17:58 +0000

    Dto to represent a row

---
+++ PDFGenerator/SampleDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDFGenerator
+{
+    class SampleDto//dto to represent a row in a database table. 
+    {
+        public int getSUN { get; set; }
+
+        public int MyProperty { get; set; }
+
+        public String getFirstName { get; set; }
+
+        public String getSecondName { get; set; }
+
+        public int getFirstYearGrade { get; set; }
+
+        public int getSecondYearGrade { get; set; }
+
+        public String getUsername { get; set; }
+
+        public String getEmail { get; set; }
+
+        public int getProgressCheck { get; set; }
+    }
+}
